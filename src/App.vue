ef16fa0b23dadc7376d13ce661fe4a8ec3b0498c
duanpeipei <duanpei0921@outlook.com>
2021-04-21 10:24:45 +0800

    demo

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+
+<template>
+  <MessageSvg />
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+import MessageSvg from './assets/example.svg'
+
+export default defineComponent({
+  components: {
+    MessageSvg
+  },
+});
+</script>
